f97577032cf904331613606c15a3ec06ce4dd25b
uldom <muldom@gmail.com>
2021-06-25 20:57:49 -0500

    app.py file in progress

--- app.py
+++ app.py
@@ -23,4 +23,21 @@ Station = Base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
+# Flask
+app = Flask(__name__)
 
+# Flask Routes
+@app.route("/")
+def Home():
+    """List all available API routes"""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br>"
+    )
+    
+if __name__ == '__main__':
+    app.run(debug=True)
