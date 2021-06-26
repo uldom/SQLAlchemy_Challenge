08c33b69abb307349a7a0bbdd0c6b5775da49147
uldom <muldom@gmail.com>
2021-06-25 20:09:24 -0500

    app.py file created

---
+++ app.py
@@ -0,0 +1,6 @@
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+from flask import Flask, jsonify
