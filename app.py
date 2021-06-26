bad5bf9d52d34b63b39dbdd36cbdd3b89fa7894a
uldom <muldom@gmail.com>
2021-06-25 20:34:34 -0500

    app.py file in progress

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 import numpy as np
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
+
 from flask import Flask, jsonify
+
+# Data Base setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Reflect an existing database into a new model
+Base = automap_base()
+
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+
