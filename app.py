9f073dc30ed0251b20d0be1319449a84279da396
uldom <muldom@gmail.com>
2021-06-26 17:24:51 -0500

    stations route

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from sqlalchemy import create_engine, func, inspect
 
 from flask import Flask, jsonify
 
+import datetime as dt
+from datetime import datetime
+from datetime import date, timedelta
+
 # Data Base setup
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
@@ -38,6 +42,43 @@ def Home():
         f"/api/v1.0/&lt;start&gt;<br>"
         f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br>"
     )
-    
+# Precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create session from Python to DB
+    session = Session(engine)
+    # Defining Last year
+    latest_day = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    latest_day = datetime.strptime(latest_day, '%Y-%m-%d')
+    strptime_dt = latest_day -timedelta(days=365)
+    # Query the last 12 months precipitation
+    one_year = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date>=strptime_dt).all()
+    session.close()
+    # Convert list of tuples into normal list
+    prec = []
+    for date, prcp in one_year:
+        p_dict = {}
+        p_dict["Date"] = date
+        p_dict["Precipitation"] = prcp
+        prec.append(p_dict)
+    return jsonify(prec)
+
+# Stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create session from Python to DB
+    session = Session(engine)
+    # Query list of stations
+    stations_list=session.query(Station.station).all()
+    session.close()
+    # Convert list of tuples into normal list
+    stations = list(np.ravel(stations_list))
+    return jsonify(stations)
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
