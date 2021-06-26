bee980b10e050aa78c071c969c982b7fd2b60ffa
uldom <muldom@gmail.com>
2021-06-26 18:09:50 -0500

    Trip Average Temperature

--- app.py
+++ app.py
@@ -75,10 +75,41 @@ def stations():
     stations = list(np.ravel(stations_list))
     return jsonify(stations)
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create session from Python to DB
+    session = Session(engine)
+    # Query most active station
+    high_tobs = session.query(Measurement.station, func.count(Measurement.tobs)).group_by(Measurement.station).order_by(func.count(Measurement.tobs).desc()).all()
+    high_tobs_station = high_tobs[0][0]
+    # Defining Last year
+    latest_day = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    latest_day = datetime.strptime(latest_day, '%Y-%m-%d')
+    strptime_dt = latest_day -timedelta(days=365)
+    # Query the last 12 months of temperature observation data for this station
+    t_obs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == high_tobs_station).filter(Measurement.date >= strptime_dt).order_by(Measurement.date).all()
+    session.close()
+    # Convert list of tuples into normal list
+    lastyear_tobs = list(np.ravel(t_obs))
+    return jsonify(lastyear_tobs)
 
+@app.route("/api/v1.0/<start>")
+def start(start = None):
+    # Create session from Python to DB
+    session = Session(engine)
+    # Selection
+    select_start = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).group_by(Measurement.date).all()
+    select_start = list(select_start)
+    return jsonify(select_start)
 
-
-
+@app.route("/api/v1.0/<start>/<end>")
+def end(start = None, end = None):
+    # Create session from Python to DB
+    session = Session(engine)
+    # Selection
+    select_end = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start, Measurement.date <= end).group_by(Measurement.date).all()
+    select_end  = list(select_end)
+    return jsonify(select_end)
 
 if __name__ == '__main__':
     app.run(debug=True)
